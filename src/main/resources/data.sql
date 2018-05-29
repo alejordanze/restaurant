c0cd120c67edc5fc54e2000999dea1524e6573b2
Miguel Alejandro Jordan Zeballos <alejandrojordan@Miguels-MacBook-Air.local>
2018-05-29 08:46:10 -0400

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
-INSERT INTO country(id, name) VALUES (3, 'Bolivia');
-
+INSERT INTO country(id, name) VALUES (1, 'Bolivia');
+INSERT INTO country(id, name) VALUES (2, 'Estados Unidos');
+INSERT INTO city(id, name, country_id) VALUES (1, 'Cochabamba', 1);
+INSERT INTO city(id, name, country_id) VALUES (2, 'Santa Cruz', 1);
+INSERT INTO city(id, name, country_id) VALUES (3, 'Maryland', 2);
+INSERT INTO city(id, name, country_id) VALUES (4, 'Virginia', 2);
 
 
 
